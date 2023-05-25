7832c18e648cd44e3595740a73057804ba0d0b1e
mattsuzuki <matthewjamessuzuki@gmail.com>
2023-05-24 19:44:14 -0700

    pm

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const calculateResults = (event) => {
         event.preventDefault();  // Add this line to prevent form submission
 
-        const onesValue = onesInput.value ? parseFloat(onesInput.value) * 1.2 / 8 : 0;
-        const twosValue = twosInput.value ? parseFloat(twosInput.value) * 1.7 / 8 : 0;
-        const threesValue = threesInput.value ? parseFloat(threesInput.value) * 2.2 / 8 : 0;
+        const onesValue = onesInput.value ? parseFloat(onesInput.value) * .75 / 8 : 0;
+        const twosValue = twosInput.value ? parseFloat(twosInput.value) * 1.0 / 8 : 0;
+        const threesValue = threesInput.value ? parseFloat(threesInput.value) * 1.40 / 8 : 0;
         const oneToOneValue = oneToOneInput.value ? parseFloat(oneToOneInput.value) : 0;
 
         resultsSpan.textContent = `Ones: ${onesValue.toFixed(4)}, Twos: ${twosValue.toFixed(4)}, Threes: ${threesValue.toFixed(4)}, One to One's: ${oneToOneValue.toFixed(4)}`;
